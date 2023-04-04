6bb19ab1aa3d2240eb36c50c665ef19302b06b35
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2023-04-04 14:49:41 +0600

    Module 1 Upload

--- Module 1/print.c
+++ Module 1/print.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    printf("Hello World\t\t\tNew Tab e Jao");
     printf("Hello World");
     return 0;
 }
 
+// \n - new line 
+// \t - tab            
+// Hello World
+// Now I am a programmer
