6076541d0e2381a122213b5401999e511a78b58e
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2023-04-26 01:28:47 +0600

    Module 11.5 Uploaded

--- Module 11.5/test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include <string.h>
-int main()
-{
-    printf("%c%c%c%c%c%c%c%c%c%c%c%c",69,105,100,32,77,117,98,97,114,97,107,33);
-    return 0;
-}
-
-// printf("%c%c%c%c%c%c%c%c%c%c%c%c",69,105,100,32,77,117,98,97,114,97,107,33);
